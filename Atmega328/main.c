1f3042a339a3046b027b99d5fa006609eaacafa9
EricMiddleton1 <ericm@iastate.edu>
2015-11-12 13:19:40 -0600

    Update main.c

--- Atmega328/main.c
+++ Atmega328/main.c
@@ -12,19 +12,20 @@ int main(void)
 {
     SoundPlayer sp;
 	
-	//Use PORTB 0-3 (arduino 8-11)
-	//8 - reset
-	//9 - data
-	//10 - clock
-	//11 - busy
-	SoundPlayer_Init(&sp, &PORTB, 0, 1, 2, 3);
+	DDRB |= 1 << 5;
+	
+	//Use PORTD 2-5 (arduino 2-5)
+	//2 - reset
+	//3 - data
+	//4 - clock
+	//5 - busy
+	SoundPlayer_Init(&sp, &PORTD, 2, 3, 4, 5);
 	
 	while(1) {
 		static int i = 0;
+		
 		SoundPlayer_SetTrack(&sp, i++);
 		Util_WaitMillis(10);
 		SoundPlayer_Play(&sp);
-		Util_WaitMillis(2000);
 	}
 }
-
